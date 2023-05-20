78f59e5e19e9d5d85e2c57f6d1a64a715eb22035
AlexanderValencia21 <alexandervalencia0121@gmail.com>
2023-05-19 21:34:12 -0500

    Adding project and Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">Egypt 3D</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/home">Inicio</a>
+        <a href="/modules">Modulos</a>
+        <a href="/resgister">Registrate</a>
+        <a href="/login">Inicia Sesion</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
