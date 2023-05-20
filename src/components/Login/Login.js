a056901c6e695112a5b5a234104a796a12f9fb13
MarlonAnacona <marlon.andres.anacona@gmail.com>
2023-05-19 22:57:41 -0500

    added login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,20 @@
+import React from 'react';
+
+import './Login.css'
+
 export function Login(){
-    return(<div>
-        Login
-    </div>)
+    return( <div className="login-container">
+    <form className="login-form">
+    <h2 className="login-title">Iniciar Sesión</h2>
+
+      <div className="form-group ">
+        <input className='textfield'type="text" id="username" placeholder='Correo Electronico'/>
+      </div>
+      <div className="form-group letter">
+        <input className='textfield' type="password" id="password" placeholder='Contraseña' />
+      </div>
+      <button type="submit" className="login-button">Ingresar</button>
+    </form>
+  </div>
+    )
 }
