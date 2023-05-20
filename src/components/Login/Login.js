16582b87e5ef7efb47632291012a3348b20494db
AlexanderValencia21 <alexandervalencia0121@gmail.com>
2023-05-20 01:03:42 -0500

    Fixing styles

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import './Login.css'
+import "./Login.css";
 
-export function Login(){
-    return( <div className="login-container">
-    <form className="login-form">
-    <h2 className="login-title">Iniciar Sesión</h2>
+export function Login() {
+  return (
+    <div className="login-container">
+      <form className="login-form">
+        <h2 className="login-title">Iniciar Sesión</h2>
+        <h4 className="login-text">Un gusto tenerte de vuelta!</h4>
 
-      <div className="form-group ">
-        <input className='textfield'type="text" id="username" placeholder='Correo Electronico'/>
-      </div>
-      <div className="form-group letter">
-        <input className='textfield' type="password" id="password" placeholder='Contraseña' />
-      </div>
-      <button type="submit" className="login-button">Ingresar</button>
-    </form>
-  </div>
-    )
+        <div className="form-group ">
+          <input
+            className="textfield"
+            type="text"
+            id="username"
+            placeholder="Correo Electronico"
+          />
+        </div>
+        <div className="form-group letter">
+          <input
+            className="textfield"
+            type="password"
+            id="password"
+            placeholder="Contraseña"
+          />
+        </div>
+        <button type="submit" className="login-button">
+          Ingresar
+        </button>
+      </form>
+    </div>
+  );
 }
