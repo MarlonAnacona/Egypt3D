eef7ae6092adf1a79866e2ff1a1bedb632b798c2
DiazSSJ <juans220304@gmail.com>
2023-05-24 11:21:18 -0500

    Visualizacion de modulos y formulario de registro

--- src/components/Modules/Modules.js
+++ src/components/Modules/Modules.js
@@ -1,5 +1,36 @@
-export function Modules(){
-    return (<div>
-        Modulos
-    </div>)
+import { useNavigate } from 'react-router-dom';
+import './Modules.css'
+
+export function Modules() {
+  const navigate = useNavigate();
+
+  const handleClick = (moduleId) => {
+    const moduleLink = `/modulo${moduleId}`;
+    navigate(moduleLink);
+  };
+
+  return (
+    <div className="card-container">
+      <div className="card" onClick={() => handleClick(1)}>
+        <h3>Módulo 1</h3>
+        <p>Descripción del módulo 1...</p>
+      </div>
+      <div className="card" onClick={() => handleClick(2)}>
+        <h3>Módulo 2</h3>
+        <p>Descripción del módulo 2...</p>
+      </div>
+      <div className="card" onClick={() => handleClick(3)}>
+        <h3>Módulo 3</h3>
+        <p>Descripción del módulo 3...</p>
+      </div>
+      <div className="card" onClick={() => handleClick(4)}>
+        <h3>Módulo 4</h3>
+        <p>Descripción del módulo 4...</p>
+      </div>
+      <div className="card" onClick={() => handleClick(5)}>
+        <h3>Módulo 5</h3>
+        <p>Descripción del módulo 5...</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,23 @@
-export function Register(){
-    return(<div>
-        Register
-    </div>)
+import React from 'react';
+import './Register.css';
+
+export function Register() {
+  return (
+    <div className="register-container">
+      <form className="register-form">
+        <h2 className="register-title">Registro</h2>
+
+        <div className="form-group">
+          <input className="textfield" type="text" id="name" placeholder="Nombre" />
+        </div>
+        <div className="form-group">
+          <input className="textfield" type="text" id="email" placeholder="Correo Electrónico" />
+        </div>
+        <div className="form-group">
+          <input className="textfield" type="password" id="password" placeholder="Contraseña" />
+        </div>
+        <button type="submit" className="register-button">Registrarse</button>
+      </form>
+    </div>
+  );
 }
