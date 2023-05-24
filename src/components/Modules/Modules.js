9abe8458189989bf81b6f86c1243df7c5af9ec70
DiazSSJ <juans220304@gmail.com>
2023-05-24 11:36:00 -0500

    modulos

--- src/components/Modules/Modules.js
+++ src/components/Modules/Modules.js
@@ -12,23 +12,23 @@ export function Modules() {
   return (
     <div className="card-container">
       <div className="card" onClick={() => handleClick(1)}>
-        <h3>Módulo 1</h3>
+        <h3>Historia</h3>
         <p>Descripción del módulo 1...</p>
       </div>
       <div className="card" onClick={() => handleClick(2)}>
-        <h3>Módulo 2</h3>
+        <h3>Arquitectura</h3>
         <p>Descripción del módulo 2...</p>
       </div>
       <div className="card" onClick={() => handleClick(3)}>
-        <h3>Módulo 3</h3>
+        <h3>Mitología</h3>
         <p>Descripción del módulo 3...</p>
       </div>
       <div className="card" onClick={() => handleClick(4)}>
-        <h3>Módulo 4</h3>
+        <h3>Arte</h3>
         <p>Descripción del módulo 4...</p>
       </div>
       <div className="card" onClick={() => handleClick(5)}>
-        <h3>Módulo 5</h3>
+        <h3>Costumbres </h3>
         <p>Descripción del módulo 5...</p>
       </div>
     </div>
