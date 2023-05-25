968488bf7d39cc234f2206ed41e8bf60434fef53
juanBailon <66891215+juanbailon@users.noreply.github.com>
2023-05-25 21:12:57 +0000

    creating user profile model

--- backend_egypt/backend_egypt_3d/backend_egypt_3d/settings.py
+++ backend_egypt/backend_egypt_3d/backend_egypt_3d/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
 
     "corsheaders",
     'rest_framework',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -178,9 +181,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+AUTH_USER_MODEL = 'users.UserProfile'
