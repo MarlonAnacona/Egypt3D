91d44aa3b9e7dd456ac32ca08b44504b28471de5
juanBailon <66891215+juanbailon@users.noreply.github.com>
2023-05-25 18:27:08 +0000

    setting up DRF

--- backend_egypt/backend_egypt_3d/backend_egypt_3d/settings.py
+++ backend_egypt/backend_egypt_3d/backend_egypt_3d/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "corsheaders",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,16 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "backend_egypt_3d.urls"
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 
 TEMPLATES = [
     {
