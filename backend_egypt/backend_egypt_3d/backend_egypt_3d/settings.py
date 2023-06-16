3178473dc540e1df84df7a301ccffc68db367b32
juanBailon <juanojo2001.5@gmail.com>
2023-06-16 13:51:20 -0500

    changing from sqlite to postgres

--- backend_egypt/backend_egypt_3d/backend_egypt_3d/settings.py
+++ backend_egypt/backend_egypt_3d/backend_egypt_3d/settings.py
@@ -145,8 +145,12 @@ WSGI_APPLICATION = "backend_egypt_3d.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
