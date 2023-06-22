0d1f68574f5301d8385c37b723c81fff6a760a85
MarlonAnacona <marlon.andres.anacona@gmail.com>
2023-06-22 12:15:57 -0500

    added cards about we

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,55 @@
-import styles from './About.module.css';
+import styles from './about.module.css';
 
 export function About(){
-    return (<div className={styles['inicio']}>
-        Trabajando en sobre nosotros :3
-    </div>)
+    return (
+        <div className={styles.inicio}>
+            <a href="https://www.mythrillfiction.com/the-dark-rider" alt="Mythrill" target="_blank">
+                <div className={styles.card}>
+                    <div className={styles.wrapper}>
+                        <img src="https://i.blogs.es/847a54/slides/1366_2000.jpg" 
+                             className={styles.coverImage} 
+                        />
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png" 
+                         className={styles.title} 
+                    />
+                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp" 
+                         className={styles.character} 
+                    />
+                </div>
+            </a>
+
+            <a href="https://www.mythrillfiction.com/force-mage" alt="Mythrill" target="_blank">
+                <div className={styles.card}>
+                    <div className={styles.wrapper}>
+                        <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" 
+                             className={styles.coverImage} 
+                        />
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png" 
+                         className={styles.title} 
+                    />
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp" 
+                         className={styles.character} 
+                    />
+                </div>
+            </a>
+            
+            <a href="https://www.mythrillfiction.com/force-mage" alt="Mythrill" target="_blank">
+                <div className={styles.card}>
+                    <div className={styles.wrapper}>
+                        <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" 
+                             className={styles.coverImage} 
+                        />
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png" 
+                         className={styles.title} 
+                    />
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp" 
+                         className={styles.character} 
+                    />
+                </div>
+            </a>
+        </div>  
+    )
 }
