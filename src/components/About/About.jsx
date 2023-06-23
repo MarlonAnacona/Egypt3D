01f9b6481cc15c3f08c21227a1d3b18be292756c
MarlonAnacona <marlon.andres.anacona@gmail.com>
2023-06-22 19:12:01 -0500

    added cards presentation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,55 +1,38 @@
-import styles from './about.module.css';
+import styles from "./about.module.css";
 
-export function About(){
-    return (
-        <div className={styles.inicio}>
-            <a href="https://www.mythrillfiction.com/the-dark-rider" alt="Mythrill" target="_blank">
-                <div className={styles.card}>
-                    <div className={styles.wrapper}>
-                        <img src="https://i.blogs.es/847a54/slides/1366_2000.jpg" 
-                             className={styles.coverImage} 
-                        />
-                    </div>
-                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png" 
-                         className={styles.title} 
-                    />
-                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp" 
-                         className={styles.character} 
-                    />
-                </div>
-            </a>
+export function About() {
+  //...
 
-            <a href="https://www.mythrillfiction.com/force-mage" alt="Mythrill" target="_blank">
-                <div className={styles.card}>
-                    <div className={styles.wrapper}>
-                        <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" 
-                             className={styles.coverImage} 
-                        />
-                    </div>
-                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png" 
-                         className={styles.title} 
-                    />
-                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp" 
-                         className={styles.character} 
-                    />
-                </div>
-            </a>
-            
-            <a href="https://www.mythrillfiction.com/force-mage" alt="Mythrill" target="_blank">
-                <div className={styles.card}>
-                    <div className={styles.wrapper}>
-                        <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" 
-                             className={styles.coverImage} 
-                        />
-                    </div>
-                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png" 
-                         className={styles.title} 
-                    />
-                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp" 
-                         className={styles.character} 
-                    />
-                </div>
-            </a>
-        </div>  
-    )
+  const authors = [
+    { name: "MARLON ANDRES ANACONA", image: "/images/marlonFondo.jpg", characterImage: "/images/marlon.jpg" },
+    { name: "ALEXANDER VALENCIA", image: "/images/pepa.png", characterImage: "/images/pepa2.png" },
+    { name: "GABRIEL FRANCO", image: "/images/gabriel.png", characterImage: "/images/gabo2.png" },
+    { name: "STEBAN DIAZ", image: "/images/gabriel.png", characterImage: "/images/gabo2.png" },
+    { name: "JUAN BAILON", image: "/images/gabriel.png", characterImage: "/images/gabo2.png" },
+  ];
+
+  return (
+    <div className={styles.inicio}>
+      {authors.map((author, index) => (
+        <div key={index} className={styles.card}>
+          <div className={styles.wrapper}>
+            <img
+              src={author.image}
+              alt="No Disponible"
+              className={styles.coverImage}
+            />
+          </div>
+          <div className={styles.title + " " + styles.authorTitle}>
+            {author.name}{" "}
+          </div>
+
+          <img
+            src={author.characterImage}
+            alt="No Disponible"
+            className={styles.character}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
