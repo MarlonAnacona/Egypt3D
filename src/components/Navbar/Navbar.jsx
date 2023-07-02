8d7fe6d7b72ad999e7502889404f182488a778ad
MarlonAnacona <marlon.andres.anacona@gmail.com>
2023-07-02 16:10:21 -0500

    add profile in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,55 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from './Navbar.module.css';
 import { LoginContext } from "../context/LoginContext";
+import { getImageProfile, getUser } from "../../Services/users";
+import jwt_decode from "jwt-decode";
 
 const Navbar = () => {
+
   const [isOpen, setIsOpen] = useState(false);
   const { isLogged, setIsLogged } = useContext(LoginContext);
+  const [name, setName] = useState('');
 
   const handleLogout = () => {
     // Lógica para cerrar sesión
     localStorage.removeItem("token");
     setIsLogged(false);
   };
+  const [selectedImage, setSelectedImage] = useState("/images/perfil.png");
+
+  useEffect(()=>{
+    if(isLogged){
+      const data= jwt_decode(localStorage.getItem("token"))
+
+      // Obtener los datos del usuario al cargar el componente
+      getUser(data.user_id).then((response)=>{
+        setName(response.username)
+        setSelectedImage(response.profile_image)
+        if(response.profile_image){
+          getImageProfile(response.profile_image).then((imageResponse=>{
+            setSelectedImage(imageResponse.profile_image)  
+          }))
+        }
+      })
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[isLogged])
+  
 
   return (
     <div className={styles.Navbar}>
       <img src="/images/logo.png" alt="Logo" className={styles['nav-logo']} />
       <span className={styles['nav-title']}>Egypt 3D</span>
+     {isLogged && <img src={selectedImage} alt="Avatar"className={styles['nav-logo']}/>}
+     
       <div className={`${styles['nav-items']} ${isOpen && styles.open}`}>
+       
         {/* Mostrar elementos del menú según el estado de inicio de sesión */}
+        {isLogged && <a href ="/profile" className={styles['nav-items']}>{name}</a>}
+
         {isLogged && <a href="/profile">Perfil</a>}
         {isLogged && <a href="/modules">Modulos</a>}
+        
         {isLogged && <a href="/">Inicio</a>}
         {!isLogged && <a href="/">Inicio</a>}
         {!isLogged && <a href="/register">Registrate</a>} 
